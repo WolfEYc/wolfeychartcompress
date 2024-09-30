bddc64a50e61319ba680215dc96567e38b22101b
WolfEYc <wolfeycode@gmail.com>
2024-09-29 21:56:53 -0500

    noice

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,36 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+#![feature(portable_simd)]
+#![feature(slice_as_chunks)]
+
+use std::simd::Simd;
+
+const SIMD_LANES: usize = 4;
+const SIMD_MIN_COMPRESS: usize = 65536;
+
+pub fn compress_remainder(values: &[f32]) -> Vec<u8> {
+    todo!()
+}
+
+pub fn compress_bulk(values: &[f32], total_size: usize) -> Vec<u8> {
+    let result = Vec::<u8>::with_capacity(total_size);
+    return result;
+}
+
+pub fn compress(values: &[f32]) -> Vec<u8> {
+    if values.len() < SIMD_MIN_COMPRESS {
+        return compress_remainder(values);
+    }
+
+    let remainder_size = values.len() % SIMD_LANES;
+    let bulk_size = values.len() - remainder_size;
+    let bulk_values = &values[..bulk_size];
+    let mut bulk_compressed = compress_bulk(bulk_values, values.len());
+    if remainder_size == 0 {
+        return bulk_compressed;
+    }
+    let remainder_values = &values[bulk_size..];
+    let remainder_compressed = compress_remainder(remainder_values);
+    bulk_compressed.extend(remainder_compressed);
+    return bulk_compressed;
 }
 
 #[cfg(test)]
@@ -8,7 +39,6 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        assert_eq!(3, 4);
     }
 }
